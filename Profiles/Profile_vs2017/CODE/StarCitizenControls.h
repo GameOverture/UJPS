61f7f6239969fffdb5c118f0831185496d6f42ac
Jason Knobler <jayknobler@gmail.com>
2018-06-28 23:34:17 -0400

    Cleanup wip

--- Profiles/Profile_vs2017/CODE/StarCitizenControls.h
+++ Profiles/Profile_vs2017/CODE/StarCitizenControls.h
@@ -3,102 +3,136 @@
 
 #include "WindowsKeys.h"
 #include "vJoyDevice.h"
-namespace VJOY = vJoyDevice;
+
 using uint = unsigned int;
-
-
-namespace StarCitizenControls_Keyboard
-{
-	const uint TrackIR_Precision			= Keys::Key_F13;
-	const uint TrackIR_Center				= Keys::Key_F14;
-	const uint TrackIR_Pause				= Keys::Key_F15;
-}
 
 namespace StarCitizenControls_vJoy1
 {
-	const uint AxisFlightRoll				= VJOY::X;
-	const uint AxisFlightPitch				= VJOY::Y;
-	const uint AxisFlightYaw				= VJOY::Z;
-	const uint AxisFlightStrafeLongi		= VJOY::ROTX;
-	const uint AxisFlightStrafeLat			= VJOY::ROTY;
-	const uint AxisFlightStrafeVertical		= VJOY::ROTZ;
-	const uint AxisFlightThrust				= VJOY::SLIDER0;
-	
-	const uint LandingSystemToggle			= VJOY::DX1;
-	const uint IncreasePower				= VJOY::DX2;
-	const uint DecreasePower				= VJOY::DX3;
-	const uint CycleAmmoBack				= VJOY::DX4;
-	const uint CycleAmmo					= VJOY::DX5;
-	const uint CycleRadarRange				= VJOY::DX6;
-	const uint Afterburner					= VJOY::DX7;
-	const uint Boost						= VJOY::DX8;
-	const uint SpaceBrake					= VJOY::DX9;
-	const uint DecoupledModeToggle			= VJOY::DX10;
-	const uint Autoland						= VJOY::DX11;
-	const uint QuantumDriveToggle			= VJOY::DX12;
-	const uint SwapYawRollToggle			= VJOY::DX13;
-	const uint FireGroup1					= VJOY::DX14;
-	const uint FireGroup2					= VJOY::DX15;
-	const uint GForceSafetyToggle			= VJOY::DX16;
-	const uint ComstabToggle				= VJOY::DX17;
-	const uint ESPToggle					= VJOY::DX18;
-	const uint LookBehind					= VJOY::DX19;
-	
-	const uint CycleCounterMeasures			= VJOY::DX20;
-	const uint LaunchCounterMeasures		= VJOY::DX21;
-	
-	const uint StrafeLeft					= VJOY::DX22;
-	const uint StrafeRight					= VJOY::DX23;
-	const uint StrafeUp						= VJOY::DX24;
-	const uint StrafeDown					= VJOY::DX25;
-	const uint StrafeForward				= VJOY::DX26;
-	const uint StrafeBackwards				= VJOY::DX27;
-	
-	const uint CycleHostileTargets			= VJOY::DX28;
-	const uint CycleHostileTargetsBack		= VJOY::DX29;
-	const uint CycleAllTargets				= VJOY::DX30;
-	const uint CycleAllTargetsBack			= VJOY::DX31;
-	const uint CycleFriendlyTargets			= VJOY::DX32;
-	const uint CycleFriendlyTargetsBack		= VJOY::DX33;
-	const uint CyclePinnedTargets			= VJOY::DX34;
-	const uint CyclePinnedTargetsBack		= VJOY::DX35;
-	const uint PinFocusedTarget				= VJOY::DX36;
-	const uint TargetNearestHostile			= VJOY::DX37;
-	const uint AcquireMissileLock			= VJOY::DX38;
-	
-	const uint PowerPreset1Toggle			= VJOY::DX39;
-	const uint PowerPreset2Toggle			= VJOY::DX40;
-	const uint PowerPreset3Toggle			= VJOY::DX41;
-	const uint PowerPreset1Increase			= VJOY::DX42;
-	const uint PowerPreset2Increase			= VJOY::DX43;
-	const uint PowerPreset3Increase			= VJOY::DX44;
-	const uint ResetPowerDistribution		= VJOY::DX45;
-	
-	const uint HeadlightsToggle				= VJOY::DX46;
-	const uint PersonalIdBroadCastToggle	= VJOY::DX47;
-	const uint LandingTargetLockOnToggle	= VJOY::DX48;
-	const uint LaunchMissile				= VJOY::DX49;
-	const uint MatchTargetVelocity			= VJOY::DX50;
+	const uint AxisFlightPitch								= vJoyDevice::ROTX;
+	const uint AxisFlightYaw								= vJoyDevice::ROTY;
+	const uint AxisFlightRoll								= vJoyDevice::ROTZ;
+	const uint AxisFlightStrafeLeftRight					= vJoyDevice::X;
+	const uint AxisFlightStrafeUpDown						= vJoyDevice::Y;
+	const uint AxisFlightStrafeFwdBck						= vJoyDevice::Z;
+	const uint AxisFlightThrottle							= vJoyDevice::SLIDER0;
+	const uint AxisFlightDynamicZoom						= vJoyDevice::SLIDER1;
+
+	const uint Eject										= vJoyDevice::DX1;
+	const uint ExitSeat										= vJoyDevice::DX2;
+	const uint SelfDestruct									= vJoyDevice::DX3;
+	const uint IncreaseCoolerRate							= vJoyDevice::DX4;
+	const uint DecreaseCoolerRate							= vJoyDevice::DX5;
+	const uint FlightSystemsReady							= vJoyDevice::DX6;
+	const uint OpenAllDoors									= vJoyDevice::DX7;
+	const uint CloseAllDoors								= vJoyDevice::DX8;
+	const uint LockAllDoors									= vJoyDevice::DX9;
+	const uint UnlockAllDoors								= vJoyDevice::DX10;
+
+	const uint CycleCameraView								= vJoyDevice::DX11;
+	const uint CycleCameraOrbitMode							= vJoyDevice::DX12;
+	const uint Whoops										= vJoyDevice::DX13;
+	const uint LookBehind									= vJoyDevice::DX14;
+
+	const uint Spacebrake									= vJoyDevice::DX15;
+	const uint MatchTargetVelocity							= vJoyDevice::DX16;
+	const uint ToggleDecoupledMode							= vJoyDevice::DX17;
+	const uint CycleIFCS									= vJoyDevice::DX18;
+	const uint ToggleGForceSafety							= vJoyDevice::DX19;
+	const uint ToggleCOMSTAB								= vJoyDevice::DX20;
+	const uint ToggleESP									= vJoyDevice::DX21;
+	const uint Afterburner									= vJoyDevice::DX22;
+	const uint Boost										= vJoyDevice::DX23;
+	const uint ToggleLandingGear							= vJoyDevice::DX24;
+	const uint Autoland										= vJoyDevice::DX25;
+	const uint ToggleQuantumTravelSystem					= vJoyDevice::DX26;
+	const uint QuantumDrive									= vJoyDevice::DX27;
+
+	const uint ResetAim										= vJoyDevice::DX28;
+	const uint LookAhead									= vJoyDevice::DX29;
+	const uint GimbalLock									= vJoyDevice::DX30;
+	const uint ReticleFocus									= vJoyDevice::DX31;
+	const uint CycleAllTargets								= vJoyDevice::DX32;
+	const uint CycleAllTargetsBack							= vJoyDevice::DX33;
+	const uint CycleFriendlyTarget							= vJoyDevice::DX34;
+	const uint CycleFriendlyTargetBack						= vJoyDevice::DX35;
+	const uint PinFocusedTarget								= vJoyDevice::DX36;
+	const uint CyclePinnedTargets							= vJoyDevice::DX37;
+	const uint CyclePinnedTargetsBack						= vJoyDevice::DX38;
+	const uint CycleHostileTargets							= vJoyDevice::DX39;
+	const uint CycleHostileTargetsBack						= vJoyDevice::DX40;
+	const uint TargetNearestHostile							= vJoyDevice::DX41;
+	const uint ToggleReticleMode							= vJoyDevice::DX42;
+	const uint TargetFocus									= vJoyDevice::DX43;
+	const uint ToggleMiningMode								= vJoyDevice::DX44;
+	const uint ToggleScanningMode							= vJoyDevice::DX45;
+	const uint ScanningRadarPing							= vJoyDevice::DX46;
+	const uint ScanningIncreaseRadarAngle					= vJoyDevice::DX47;
+	const uint ScanningDecreaseRadarAngle					= vJoyDevice::DX48;
+
+	const uint FireWeaponGroup1								= vJoyDevice::DX49;
+	const uint FireWeaponGroup2								= vJoyDevice::DX50;
 }
 
 namespace StarCitizenControls_vJoy2
 {
-	const uint AxisGroundThrust				= VJOY::X;
-	const uint AxisGroundLat				= VJOY::Y;
-	
-	const uint ResetShieldsLevels			= VJOY::DX1;
-	const uint ShieldRaiseLeft				= VJOY::DX2;
-	const uint ShieldRaiseRight				= VJOY::DX3;
-	const uint ShieldRaiseFront				= VJOY::DX4;
-	const uint ShieldRaiseBack				= VJOY::DX5;
-	const uint ShieldRaiseTop				= VJOY::DX6;
-	const uint ShieldRaiseBottom			= VJOY::DX7;
-	
-	const uint FlightReady					= VJOY::DX8;
-	const uint PowerOff						= VJOY::DX9;
-	
-	const uint Brake						= VJOY::DX30;
-	const uint Horn							= VJOY::DX31;
+	const uint AxisAimLeftRight								= vJoyDevice::ROTY;
+	const uint AxisAimUpDown								= vJoyDevice::ROTX;
+	const uint AxisOnFootLeftRight							= vJoyDevice::X;
+	const uint AxisOnFootFwdBck								= vJoyDevice::Z;
+	const uint AxisVehicleFwdBck							= vJoyDevice::SLIDER0;
+	const uint AxisVehicleLeftRight							= vJoyDevice::SLIDER1;
+
+	const uint CycleWeaponAmmo								= vJoyDevice::DX1;
+	const uint CycleWeaponAmmoBack							= vJoyDevice::DX2;
+	const uint AcquireMissileLock							= vJoyDevice::DX3;
+	const uint LaunchMissile								= vJoyDevice::DX4;
+
+	const uint LaunchCountermeasure							= vJoyDevice::DX5;
+	const uint CycleCountermeasureAmmo						= vJoyDevice::DX6;
+	const uint CycleCountermeasureAmmoBack					= vJoyDevice::DX7;
+	const uint ShieldRaiseFront								= vJoyDevice::DX8;
+	const uint ShieldRaiseBack								= vJoyDevice::DX9;
+	const uint ShieldRaiseLeft								= vJoyDevice::DX10;
+	const uint ShieldRaiseRight								= vJoyDevice::DX11;
+	const uint ShieldRaiseTop								= vJoyDevice::DX12;
+	const uint ShieldRaiseBottom							= vJoyDevice::DX13;
+	const uint ShieldResetLevels							= vJoyDevice::DX14;
+
+	const uint PowerPreset1									= vJoyDevice::DX15;
+	const uint PowerPreset2									= vJoyDevice::DX16;
+	const uint PowerPreset3									= vJoyDevice::DX17;
+	const uint ResetPowerDistribution						= vJoyDevice::DX18;
+	const uint IncreasePower								= vJoyDevice::DX19;
+	const uint DecreasePower								= vJoyDevice::DX20;
+	const uint IncreasePowerMax								= vJoyDevice::DX21;
+	const uint DecreasePowerMin								= vJoyDevice::DX22;
+	const uint TogglePowerPreset1							= vJoyDevice::DX23;
+	const uint TogglePowerPreset2							= vJoyDevice::DX24;
+	const uint TogglePowerPreset3							= vJoyDevice::DX25;
+	const uint TogglePower									= vJoyDevice::DX26;
+
+	const uint TogglePersonalIdentificationBroadcastSystem	= vJoyDevice::DX27;
+	const uint CycleRadarRange								= vJoyDevice::DX28;
+
+	const uint Scoreboard									= vJoyDevice::DX29;
+	const uint Map											= vJoyDevice::DX30;
+
+	const uint ToggleLights									= vJoyDevice::DX31;
+
+	const uint LeanLeft										= vJoyDevice::DX32;
+	const uint LeanRight									= vJoyDevice::DX33;
+
+	const uint VehicleHorn									= vJoyDevice::DX34;
+	const uint VehicleBrake									= vJoyDevice::DX35;
+
+	const uint CameraIncreaseFov							= vJoyDevice::DX36;
+	const uint CameraDecreaseFov							= vJoyDevice::DX37;
+	const uint CameraIncreaseDof							= vJoyDevice::DX38;
+	const uint CameraDecreaseDof							= vJoyDevice::DX39;
+
+	const uint TrackIR_Precision							= vJoyDevice::DX40;
+	const uint TrackIR_Center								= vJoyDevice::DX41;
+	const uint TrackIR_Pause								= vJoyDevice::DX42;
 }
 
 #endif
